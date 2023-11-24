3674a4fce67bb3fc16effdc1cb37cb91e5e9f989
white-gyu <white_gyu@naver.com>
2023-11-24 17:55:09 +0900

    feat: objectmapper bean 등록

---
+++ src/main/kotlin/com/newspeed/user/global/config/JacksonConfig.kt
@@ -0,0 +1,16 @@
+package com.newspeed.user.global.config
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
+import com.fasterxml.jackson.module.kotlin.registerKotlinModule
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class JacksonConfig {
+
+    @Bean
+    fun objectMapper(): ObjectMapper = ObjectMapper()
+        .registerModule(JavaTimeModule())
+        .registerKotlinModule()
+}
