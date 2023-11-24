5de37dfa4d3097448f1f7152abe9a90a8339bb92
white-gyu <white_gyu@naver.com>
2023-11-24 17:12:35 +0900

    chore: project init

---
+++ src/test/kotlin/com/newspeed/user/NewspeedUserServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.newspeed.user
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class NewspeedUserServerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
