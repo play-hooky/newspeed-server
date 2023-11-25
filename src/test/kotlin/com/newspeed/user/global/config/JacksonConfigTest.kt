e8b9d793f0d1225d060379238a7e1d89432ad6c0
white-gyu <white_gyu@naver.com>
2023-11-25 15:33:17 +0900

    test: 실패하는 테스트 코드 추가 ci 테스트

---
+++ src/test/kotlin/com/newspeed/user/global/config/JacksonConfigTest.kt
@@ -0,0 +1,12 @@
+package com.newspeed.user.global.config
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+class JacksonConfigTest {
+
+    @Test
+    fun `실패_테스트`() {
+        assertEquals(1, 2)
+    }
+}
