c1894f8ea0c6e0b44d9dfd7b17b6f3a785028c5d
white-gyu <white_gyu@naver.com>
2023-11-25 22:57:12 +0900

    test: 단위 테스트 템플릿 정의

---
+++ src/test/kotlin/com/newspeed/user/template/UnitTestTemplate.kt
@@ -0,0 +1,9 @@
+package com.newspeed.user.template
+
+import org.junit.jupiter.api.DisplayNameGeneration
+import org.junit.jupiter.api.DisplayNameGenerator.ReplaceUnderscores
+
+
+@DisplayNameGeneration(ReplaceUnderscores::class)
+@SuppressWarnings("NonAsciiCharacters")
+interface UnitTestTemplate
