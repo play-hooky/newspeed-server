50cbd25c31027d43376e3aeed21e96bc5c3ac925
white-gyu <white_gyu@naver.com>
2023-11-26 01:47:58 +0900

    feat: kakao login 로직 내 jwt 발행 로직 구현 완료

---
+++ src/main/kotlin/com/newspeed/user/domain/auth/application/AuthFacade.kt
@@ -0,0 +1,39 @@
+package com.newspeed.user.domain.auth.application
+
+import com.newspeed.user.domain.auth.api.request.KakaoLoginRequest
+import com.newspeed.user.domain.auth.api.response.KakaoLoginResponse
+import com.newspeed.user.domain.auth.domain.LoginPlatform
+import com.newspeed.user.domain.auth.domain.OAuth2User
+import com.newspeed.user.domain.auth.feign.OAuth2Clients
+import com.newspeed.user.domain.jwt.application.JwtAuthProvider
+import org.springframework.stereotype.Component
+
+@Component
+class AuthFacade(
+    private val oAuth2Clients: OAuth2Clients,
+    private val jwtAuthProvider: JwtAuthProvider
+) {
+
+    fun kakaoLogin(
+        kakaoLoginRequest: KakaoLoginRequest
+    ): KakaoLoginResponse {
+        val oAuth2User = getKakaoUser(kakaoLoginRequest)
+
+        // todo: user entity 회원가입 및 로그인
+        val userId = 1L
+
+        val accessToken = jwtAuthProvider.provideAccessToken(oAuth2User.toAuthPayload(userId))
+        val refreshToken = jwtAuthProvider.provideRefreshToken(oAuth2User.toAuthPayload(userId))
+
+        return KakaoLoginResponse(
+            accessToken = accessToken,
+            refreshToken = refreshToken,
+            userId = userId
+        )
+    }
+
+    private fun getKakaoUser(
+        kakaoLoginRequest: KakaoLoginRequest
+    ): OAuth2User = oAuth2Clients.getClient(LoginPlatform.KAKAO)
+        .getOAuth2User(kakaoLoginRequest.authorizationCode)
+}
