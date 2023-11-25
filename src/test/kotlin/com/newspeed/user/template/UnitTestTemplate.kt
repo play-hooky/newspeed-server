43fe4fec14afe3b865f5cbe7a73a3296b0a9cf80
white-gyu <white_gyu@naver.com>
2023-11-26 01:42:43 +0900

    test: 단위 테스트 템플릿 내 mockito 활성화

--- src/test/kotlin/com/newspeed/user/template/UnitTestTemplate.kt
+++ src/test/kotlin/com/newspeed/user/template/UnitTestTemplate.kt
@@ -2,8 +2,11 @@ package com.newspeed.user.template
 
 import org.junit.jupiter.api.DisplayNameGeneration
 import org.junit.jupiter.api.DisplayNameGenerator.ReplaceUnderscores
+import org.junit.jupiter.api.extension.ExtendWith
+import org.mockito.junit.jupiter.MockitoExtension
 
 
 @DisplayNameGeneration(ReplaceUnderscores::class)
 @SuppressWarnings("NonAsciiCharacters")
+@ExtendWith(MockitoExtension::class)
 interface UnitTestTemplate
