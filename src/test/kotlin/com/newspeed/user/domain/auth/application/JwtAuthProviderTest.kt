32bacdb996f049c9bd2be770770e84ae1916072e
white-gyu <white_gyu@naver.com>
2023-11-25 22:57:20 +0900

    test: jwt 발행 로직 테스트 코드 작성

---
+++ src/test/kotlin/com/newspeed/user/domain/auth/application/JwtAuthProviderTest.kt
@@ -0,0 +1,63 @@
+package com.newspeed.user.domain.auth.application
+
+import com.newspeed.user.domain.auth.domain.AuthPayload
+import com.newspeed.user.domain.auth.domain.JwtConfigProperties
+import com.newspeed.user.domain.auth.domain.LoginPlatform
+import com.newspeed.user.domain.auth.domain.Role
+import com.newspeed.user.template.UnitTestTemplate
+import io.jsonwebtoken.Jwts
+import org.junit.jupiter.api.Test
+
+class JwtAuthProviderTest: UnitTestTemplate {
+    private val jwtConfigProperties = JwtConfigProperties(
+        header = "Authorization",
+        issuer = "play-hooky",
+        secretKey = "798B910610FBB30ADF39647971CF3D4FA4936E5D7697B304F5A4C3A8378A1AFE",
+        refreshTokenExpirySeconds = 2592000,
+        accessTokenExpirySeconds = 86400
+    )
+
+    private val jwtAuthProvider = JwtAuthProvider(
+        jwtConfigProperties = jwtConfigProperties
+    )
+
+    @Test
+    fun access_token_생성을_성공한다() {
+        // given
+        val authPayload = AuthPayload(
+            userId = 1L,
+            role = Role.USER,
+            loginPlatform = LoginPlatform.KAKAO,
+            email = "test@kakao.com"
+        )
+        val jwtParser = Jwts.parserBuilder()
+            .setSigningKey(jwtConfigProperties.secretKey.toByteArray())
+            .build()
+
+        // when
+        val token = jwtAuthProvider.provideAccessToken(authPayload)
+
+        // then
+        assert(jwtParser.isSigned(token))
+    }
+
+    @Test
+    fun refresh_token_생성을_성공한다() {
+        // given
+        val authPayload = AuthPayload(
+            userId = 1L,
+            role = Role.USER,
+            loginPlatform = LoginPlatform.KAKAO,
+            email = "test@kakao.com"
+        )
+        val jwtParser = Jwts.parserBuilder()
+            .setSigningKey(jwtConfigProperties.secretKey.toByteArray())
+            .build()
+
+        // when
+        val token = jwtAuthProvider.provideAccessToken(authPayload)
+
+        // then
+        assert(jwtParser.isSigned(token))
+    }
+}
