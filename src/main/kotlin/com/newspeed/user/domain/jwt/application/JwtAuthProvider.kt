70d6e0122e030d5a5277b866da684668e0c7ccdd
white-gyu <white_gyu@naver.com>
2023-11-26 01:42:24 +0900

    feat: jwt 발행 로직 구현

---
+++ src/main/kotlin/com/newspeed/user/domain/jwt/application/JwtAuthProvider.kt
@@ -0,0 +1,44 @@
+package com.newspeed.user.domain.jwt.application
+
+import com.newspeed.user.domain.auth.domain.AuthPayload
+import com.newspeed.user.domain.jwt.domain.JwtConfigProperties
+import io.jsonwebtoken.JwtBuilder
+import io.jsonwebtoken.SignatureAlgorithm
+import org.springframework.stereotype.Component
+import java.util.*
+
+@Component
+class JwtAuthProvider(
+    private val jwtConfigProperties: JwtConfigProperties
+) {
+
+    fun provideRefreshToken(
+        authPayload: AuthPayload
+    ): String {
+        val now = Date()
+
+        return authPayload
+            .toJwtClaims()
+            .build(now, jwtConfigProperties.refreshTokenExpirySeconds, jwtConfigProperties.secretKey)
+    }
+
+    fun provideAccessToken(
+        authPayload: AuthPayload
+    ): String {
+        val now = Date()
+
+        return authPayload
+            .toJwtClaims()
+            .build(now, jwtConfigProperties.accessTokenExpirySeconds, jwtConfigProperties.secretKey)
+    }
+}
+
+private fun JwtBuilder.build(
+    now: Date,
+    expiration: Long,
+    key: String
+): String = this
+    .setIssuedAt(now)
+    .setExpiration(Date(now.time + expiration * 1000))
+    .signWith(SignatureAlgorithm.HS256, key)
+    .compact()
