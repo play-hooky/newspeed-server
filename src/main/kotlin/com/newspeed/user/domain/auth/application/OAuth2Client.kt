48b44316d2f62751d5d41584ba8a49fd8bd7911a
white-gyu <white_gyu@naver.com>
2023-11-26 01:43:29 +0900

    feat: oauth login 사용자 정보 조회 기능 추상화

--- src/main/kotlin/com/newspeed/user/domain/auth/application/OAuth2Client.kt
+++ src/main/kotlin/com/newspeed/user/domain/auth/application/OAuth2Client.kt
@@ -1,8 +1,11 @@
 package com.newspeed.user.domain.auth.application
 
+import com.newspeed.user.domain.auth.domain.LoginPlatform
 import com.newspeed.user.domain.auth.domain.OAuth2User
 
 interface OAuth2Client {
+
+    fun getLoginPlatform(): LoginPlatform
 
     fun getOAuth2User(accessToken: String): OAuth2User
 }
