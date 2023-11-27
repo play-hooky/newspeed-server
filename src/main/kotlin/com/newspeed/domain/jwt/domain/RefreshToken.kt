2a74ec758d36f34397e83951c93d27a0eab12494
white-gyu <white_gyu@naver.com>
2023-11-27 18:30:01 +0900

    feat: refresh token entity, repository 구현

---
+++ src/main/kotlin/com/newspeed/domain/jwt/domain/RefreshToken.kt
@@ -0,0 +1,17 @@
+package com.newspeed.domain.jwt.domain
+
+import org.springframework.data.redis.core.RedisHash
+import org.springframework.data.redis.core.index.Indexed
+import javax.persistence.Id
+
+@RedisHash(value = "refreshToken", timeToLive = 2592000)
+class RefreshToken(
+    @Id
+    var id: Long = 0,
+
+    @Indexed
+    var userId: Long,
+
+    @Indexed
+    var token: String
+)
