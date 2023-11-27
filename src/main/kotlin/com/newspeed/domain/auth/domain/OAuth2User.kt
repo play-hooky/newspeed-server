9df6eeec1e44fa894f9a8bd52ce6ff282fcff532
white-gyu <white_gyu@naver.com>
2023-11-27 18:29:51 +0900

    feat: oauth user -> user entity 저장 로직 구현

--- src/main/kotlin/com/newspeed/domain/auth/domain/OAuth2User.kt
+++ src/main/kotlin/com/newspeed/domain/auth/domain/OAuth2User.kt
@@ -1,17 +1,19 @@
 package com.newspeed.domain.auth.domain
 
+import com.newspeed.domain.user.domain.User
+
 data class OAuth2User(
     val platform: LoginPlatform,
     val nickname: String,
     val profileImage: String,
     val email: String
 ) {
-    fun toAuthPayload(
-        userId: Long
-    ): AuthPayload = AuthPayload(
-        userId = userId,
-        role = Role.USER,
-        loginPlatform = platform,
-        email = email
+
+    fun toUser(): User = User(
+        email = email,
+        nickname = nickname,
+        platform = platform,
+        profileImageUrl = profileImage,
+        role = Role.USER
     )
 }
