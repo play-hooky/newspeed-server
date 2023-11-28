3a5ed56d9b9f330f071209e0ac4b4e7984742af8
white-gyu <white_gyu@naver.com>
2023-11-28 15:58:41 +0900

    feat: h2, embedded redis 등 테스트 환경 구축

--- src/test/kotlin/com/newspeed/template/IntegrationTestTemplate.kt
+++ src/test/kotlin/com/newspeed/template/IntegrationTestTemplate.kt
@@ -1,6 +1,12 @@
 package com.newspeed.template
 
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
+import org.testcontainers.junit.jupiter.Testcontainers
 
 @SpringBootTest
+@Testcontainers
+@ActiveProfiles("test")
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 interface IntegrationTestTemplate
