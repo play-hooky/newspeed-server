2dab584ec1cb9a62e4cc42d847f3a91a20ba3f26
white-gyu <white_gyu@naver.com>
2023-11-28 18:36:57 +0900

    feat: 테스트 코드 내 필요한 stub 객체 생성 팩토리 구현

---
+++ src/test/kotlin/com/newspeed/factory/auth/AuthFactory.kt
@@ -0,0 +1,54 @@
+package com.newspeed.factory.auth
+
+import com.newspeed.domain.auth.domain.LoginPlatform
+import com.newspeed.domain.auth.domain.OAuth2User
+import com.newspeed.domain.auth.feign.request.KakaoOAuth2TokenRequest
+import com.newspeed.domain.auth.feign.response.KakaoOAuth2TokenResponse
+import com.newspeed.domain.auth.feign.response.KakaoOAuth2UserResponse
+
+class AuthFactory {
+    companion object {
+        const val DUMMY_EMAIL = "happyhooky@kakao.com"
+        const val DUMMY_NICKNAME = "happyhooky"
+        const val DUMMY_PROFILE_IMAGE_URL = "https://k.kakaocdn.net/14/dn/btspcwukiG5/gu6lVSl4fgjKacbmw5kZr0/o.jpg"
+
+        const val DUMMY_ACCESS_TOKEN = "hookyhookyhookyhookyhookyhooky"
+        const val DUMMY_REFRESH_TOKEN = "hookyhookyhookyhookyhookyhooky"
+
+        fun createKakaoOauth2TokenRequest(): KakaoOAuth2TokenRequest = KakaoOAuth2TokenRequest(
+            grantType = "bearer",
+            clientId = DUMMY_NICKNAME,
+            redirectUri = "https://www.newspeed.store",
+            authorizationCode = DUMMY_ACCESS_TOKEN
+        )
+
+
+        fun createKakaoOAuth2User(): OAuth2User = OAuth2User(
+            platform = LoginPlatform.KAKAO,
+            nickname = DUMMY_NICKNAME,
+            profileImage = DUMMY_PROFILE_IMAGE_URL,
+            email = DUMMY_EMAIL
+        )
+
+        fun createKakaoOAuth2TokenResponse() = KakaoOAuth2TokenResponse(
+            tokenType = "bearer",
+            accessToken = DUMMY_ACCESS_TOKEN,
+            refreshToken = DUMMY_REFRESH_TOKEN,
+            expiresIn = 21599,
+            scope = "account_email profile_image profile_nickname",
+            refreshTokenExpiresIn = 5183999
+        )
+
+        fun createKakaoOAuth2UserResponse() = KakaoOAuth2UserResponse(
+            id = "2945247360",
+            kakaoAccount = KakaoOAuth2UserResponse.KakaoAccount(
+                email = DUMMY_EMAIL,
+                profile = KakaoOAuth2UserResponse.KakaoAccount.Profile(
+                    nickname = DUMMY_NICKNAME,
+                    thumbnailImageUrl = DUMMY_PROFILE_IMAGE_URL,
+                    profileImageUrl = DUMMY_PROFILE_IMAGE_URL
+                )
+            )
+        )
+    }
+}
