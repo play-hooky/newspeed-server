b1b5dc4e6a003df82378424e77ce036fb77782e1
white-gyu <white_gyu@naver.com>
2023-11-28 15:57:46 +0900

    refactor: redis 선언적 트랜잭션 제거, 트랜잭션 해제 시 npe 발생 대응

--- src/main/kotlin/com/newspeed/domain/jwt/application/JwtService.kt
+++ src/main/kotlin/com/newspeed/domain/jwt/application/JwtService.kt
@@ -5,10 +5,8 @@ import com.newspeed.domain.jwt.domain.RefreshToken
 import com.newspeed.domain.jwt.dto.IssuedJwtDTO
 import com.newspeed.domain.jwt.repository.RefreshTokenRepository
 import org.springframework.stereotype.Service
-import javax.transaction.Transactional
 
 @Service
-@Transactional
 class JwtService(
     private val jwtAuthProvider: JwtAuthProvider,
     private val refreshTokenRepository: RefreshTokenRepository
