189b69c8aa968ff905a6b6beca4f7eff562b7903
white-gyu <white_gyu@naver.com>
2023-12-13 00:26:10 +0900

    feat: youtube 외부 검색 API 로직 구현

---
+++ src/main/kotlin/com/newspeed/domain/content/application/YoutubeSearchService.kt
@@ -0,0 +1,32 @@
+package com.newspeed.domain.content.application
+
+import com.newspeed.domain.content.api.request.ContentSearchRequest
+import com.newspeed.domain.content.api.response.ContentSearchResponse
+import com.newspeed.domain.content.config.YoutubeConfigProperties
+import com.newspeed.domain.content.domain.enums.QueryPlatform
+import com.newspeed.domain.content.feign.YoutubeClient
+import org.springframework.stereotype.Component
+
+@Component
+class YoutubeSearchService(
+    private val youtubeClient: YoutubeClient,
+    private val youtubeConfigProperties: YoutubeConfigProperties
+): ContentSearchClient {
+    override fun getQueryPlatform(): QueryPlatform = QueryPlatform.YOUTUBE
+
+    override fun search(
+        request: ContentSearchRequest
+    ): ContentSearchResponse {
+        val searchRequest = request.toYoutubeSearchRequest(youtubeConfigProperties)
+        val searchResponse = youtubeClient.search(searchRequest)
+
+        val channelRequest = searchResponse.toChannelRequest(youtubeConfigProperties)
+        val channelResponse = youtubeClient.getChannel(channelRequest)
+
+        val videoDetailRequest = searchResponse.toVideoDetailRequest(youtubeConfigProperties)
+        val videoDetailResponse = youtubeClient.getDetailContent(videoDetailRequest)
+
+        return searchResponse
+            .toContentSearchResponse(channelResponse, videoDetailResponse)
+    }
+}
