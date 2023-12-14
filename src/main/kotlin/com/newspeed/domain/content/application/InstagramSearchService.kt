492fce9637be48fd9658135eb90d8bb8297c019a
white-gyu <white_gyu@naver.com>
2023-12-14 21:43:38 +0900

    feat: instagram 검색 서비스 빈 주입

--- src/main/kotlin/com/newspeed/domain/content/application/InstagramSearchService.kt
+++ src/main/kotlin/com/newspeed/domain/content/application/InstagramSearchService.kt
@@ -3,7 +3,9 @@ package com.newspeed.domain.content.application
 import com.newspeed.domain.content.api.request.ContentSearchRequest
 import com.newspeed.domain.content.api.response.ContentSearchResponse
 import com.newspeed.domain.content.domain.enums.QueryPlatform
+import org.springframework.stereotype.Service
 
+@Service
 class InstagramSearchService: ContentSearchClient {
     override fun getQueryPlatform(): QueryPlatform = QueryPlatform.INSTAGRAM
 
