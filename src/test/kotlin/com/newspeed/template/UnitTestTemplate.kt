925c7ca6ecadfa6e82a6c7531608f41dfb77ef57
white-gyu <white_gyu@naver.com>
2024-01-03 11:20:41 +0900

    test: findByIdOrNull mocking 위한 로직 구현

--- src/test/kotlin/com/newspeed/template/UnitTestTemplate.kt
+++ src/test/kotlin/com/newspeed/template/UnitTestTemplate.kt
@@ -1,5 +1,6 @@
 package com.newspeed.template
 
+import io.mockk.junit5.MockKExtension
 import org.junit.jupiter.api.DisplayNameGeneration
 import org.junit.jupiter.api.DisplayNameGenerator.ReplaceUnderscores
 import org.junit.jupiter.api.extension.ExtendWith
@@ -9,4 +10,5 @@ import org.mockito.junit.jupiter.MockitoExtension
 @DisplayNameGeneration(ReplaceUnderscores::class)
 @SuppressWarnings("NonAsciiCharacters")
 @ExtendWith(MockitoExtension::class)
+@ExtendWith(MockKExtension::class)
 interface UnitTestTemplate
