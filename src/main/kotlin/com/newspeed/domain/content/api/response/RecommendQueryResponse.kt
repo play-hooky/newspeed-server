4c11ca4ba73faaf2881d83196567269b91ed89b8
white-gyu <white_gyu@naver.com>
2024-01-04 13:11:18 +0900

    feat: 추천 검색어 조회 API 반환값 직렬화 구현

--- src/main/kotlin/com/newspeed/domain/content/api/response/RecommendQueryResponse.kt
+++ src/main/kotlin/com/newspeed/domain/content/api/response/RecommendQueryResponse.kt
@@ -1,5 +1,9 @@
 package com.newspeed.domain.content.api.response
 
+import com.fasterxml.jackson.databind.annotation.JsonSerialize
+import com.newspeed.domain.content.application.serde.RecommendQueryResponseSerializer
+
+@JsonSerialize(using = RecommendQueryResponseSerializer::class)
 data class RecommendQueryResponse(
     val query: List<String>
 )
---
+++ src/main/kotlin/com/newspeed/domain/content/application/serde/RecommendQueryResponseSerializer.kt
@@ -0,0 +1,20 @@
+package com.newspeed.domain.content.application.serde
+
+import com.fasterxml.jackson.core.JsonGenerator
+import com.fasterxml.jackson.databind.SerializerProvider
+import com.fasterxml.jackson.databind.ser.std.StdSerializer
+import com.newspeed.domain.content.api.response.RecommendQueryResponse
+
+class RecommendQueryResponseSerializer: StdSerializer<RecommendQueryResponse>(RecommendQueryResponse::class.java) {
+    override fun serialize(
+        responseBody: RecommendQueryResponse,
+        gen: JsonGenerator,
+        provider: SerializerProvider
+    ) {
+       gen.writeStartArray()
+       responseBody
+           .query
+           .forEach{ gen.writeObject(it) }
+        gen.writeEndArray()
+    }
+}
