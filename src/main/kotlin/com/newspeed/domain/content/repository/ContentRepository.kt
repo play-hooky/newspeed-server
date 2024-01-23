1eacf285a638514859e74002138c16546f5c89b4
white-gyu <white_gyu@naver.com>
2024-01-23 19:25:28 +0900

    feat: 보관함 삭제 api 로직 구현

--- src/main/kotlin/com/newspeed/domain/content/repository/ContentRepository.kt
+++ src/main/kotlin/com/newspeed/domain/content/repository/ContentRepository.kt
@@ -1,10 +1,19 @@
 package com.newspeed.domain.content.repository
 
 import com.newspeed.domain.content.domain.Content
+import com.newspeed.domain.user.domain.User
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Modifying
+import org.springframework.data.jpa.repository.Query
 
 interface ContentRepository: JpaRepository<Content, Long> {
-    fun existsByContentIdInPlatformAndUserId(url: String, userId: Long): Boolean
+    fun existsByUserIdAndContentIdInPlatform(userId: Long, contentIdInPlatform: String): Boolean
+
+    fun findByUserAndUrl(user: User, url: String): List<Content>
 
     fun findByUserId(userId: Long): List<Content>
+
+    @Modifying
+    @Query("update Content content set content.deletedAt = now() where content.id in :ids")
+    fun deleteByIds(ids: List<Long>)
 }
