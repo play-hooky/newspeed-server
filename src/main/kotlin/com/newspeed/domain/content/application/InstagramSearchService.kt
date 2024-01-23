b532738ea334be0e6ae80db4350b5c1d7649af91
white-gyu <white_gyu@naver.com>
2024-01-23 17:04:20 +0900

    feat: dummy 컨텐츠 조회 외부 API 로직 구현

--- src/main/kotlin/com/newspeed/domain/content/application/InstagramSearchService.kt
+++ src/main/kotlin/com/newspeed/domain/content/application/InstagramSearchService.kt
@@ -3,6 +3,9 @@ package com.newspeed.domain.content.application
 import com.newspeed.domain.content.api.request.ContentSearchRequest
 import com.newspeed.domain.content.api.response.ContentSearchResponse
 import com.newspeed.domain.content.domain.enums.QueryPlatform
+import com.newspeed.domain.content.dto.ContentHostDTO
+import com.newspeed.domain.content.dto.ContentResponseDTO
+import com.newspeed.domain.content.dto.ContentYoutubeDTO
 import org.springframework.stereotype.Service
 
 @Service
@@ -13,5 +16,28 @@ class InstagramSearchService: ContentSearchClient {
         request: ContentSearchRequest
     ): ContentSearchResponse {
         TODO("Not yet implemented")
+    }
+
+    override fun search(ids: List<String>): List<ContentResponseDTO> {
+        // TODO
+
+        return listOf(
+            ContentResponseDTO(
+                platform = QueryPlatform.YOUTUBE,
+                host = ContentHostDTO(
+                    profileImgUrl = "https://www.newspeed.store/happy-hooky",
+                    nickname = "happy-hooky"
+                ),
+                youtube = ContentYoutubeDTO(
+                    id = "1",
+                    thumbnailUrl = "https://www.newspeed.store/hooky.jpg",
+                    title = "hoooky",
+                    url = "https://www.youtube.com/watch?v=1",
+                    views = "20회",
+                    todayBefore = "10일 전"
+                ),
+                instagram = null
+            )
+        )
     }
 }
--- src/test/kotlin/com/newspeed/factory/content/DummyContentClient.kt
+++ src/test/kotlin/com/newspeed/factory/content/DummyContentClient.kt
@@ -4,6 +4,7 @@ import com.newspeed.domain.content.api.request.ContentSearchRequest
 import com.newspeed.domain.content.api.response.ContentSearchResponse
 import com.newspeed.domain.content.application.ContentSearchClient
 import com.newspeed.domain.content.domain.enums.QueryPlatform
+import com.newspeed.domain.content.dto.ContentResponseDTO
 import org.springframework.stereotype.Service
 
 @Service
@@ -11,4 +12,6 @@ class DummyContentClient: ContentSearchClient {
     override fun getQueryPlatform(): QueryPlatform = QueryPlatform.NEWSPEED
 
     override fun search(request: ContentSearchRequest): ContentSearchResponse = ContentFactory.createContentSearchResponse()
+
+    override fun search(ids: List<String>): List<ContentResponseDTO> = ContentFactory.createContentResponseDTOs()
 }
