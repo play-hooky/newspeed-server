cdd9aaab2ded55ee65d4a8f8dcaa40d788d7c6f9
white-gyu <white_gyu@naver.com>
2024-01-24 17:18:26 +0900

    refactor: enum property 정의

--- src/main/kotlin/com/newspeed/global/enums/DateTimeFormats.kt
+++ src/main/kotlin/com/newspeed/global/enums/DateTimeFormats.kt
@@ -4,12 +4,10 @@ import java.sql.Time
 import java.text.SimpleDateFormat
 
 enum class DateTimeFormats(
-    value: SimpleDateFormat
+    val value: SimpleDateFormat
 ) {
     HOUR_MINUTE_TIME(SimpleDateFormat("HH:mm")),
     ;
-
-    fun value(): SimpleDateFormat = this.value()
 }
 
-fun String.toTime(): Time = Time(DateTimeFormats.HOUR_MINUTE_TIME.value().parse(this).time)
+fun String.toTime(): Time = Time(DateTimeFormats.HOUR_MINUTE_TIME.value.parse(this).time)
