4aba23bb26a01d1ef042947795ba034d5644beae
white-gyu <white_gyu@naver.com>
2024-01-25 14:40:28 +0900

    feat: 문의 내용/답변 도메인 구현

---
+++ src/main/kotlin/com/newspeed/domain/inquiry/domain/InquiryAnswer.kt
@@ -0,0 +1,45 @@
+package com.newspeed.domain.inquiry.domain
+
+import com.newspeed.global.model.BaseTimeEntity
+import org.hibernate.annotations.Comment
+import org.hibernate.annotations.SQLDelete
+import org.hibernate.annotations.Where
+import javax.persistence.*
+
+@Entity
+@Table(name = "inquiry_answer")
+@SQLDelete(sql = "UPDATE newspeed.inquiry_answer SET newspeed.inquiry_answer.deleted_at = now() WHERE id = ?")
+@Where(clause = "deleted_at is null")
+class InquiryAnswer(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    @Comment("id")
+    val id: Long = 0,
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "question_id", nullable = false, foreignKey = ForeignKey(ConstraintMode.NO_CONSTRAINT))
+    @Comment("문의 내용 id")
+    var question: InquiryQuestion,
+
+    @Column(name = "body", nullable = false, columnDefinition = "text")
+    @Comment("문의 답변")
+    var body: String,
+
+    @Column(name = "created_by", nullable = false, length = 64)
+    @Comment("답변한 사람")
+    var createdBy: String
+): BaseTimeEntity() {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as InquiryAnswer
+
+        return id == other.id
+    }
+
+    override fun hashCode(): Int {
+        return id.toInt()
+    }
+}
---
+++ src/main/kotlin/com/newspeed/domain/inquiry/domain/InquiryQuestion.kt
@@ -0,0 +1,46 @@
+package com.newspeed.domain.inquiry.domain
+
+import com.newspeed.domain.user.domain.User
+import com.newspeed.global.model.BaseTimeEntity
+import org.hibernate.annotations.Comment
+import org.hibernate.annotations.SQLDelete
+import org.hibernate.annotations.Where
+import javax.persistence.*
+
+@Entity
+@Table(name = "inquiry_question")
+@SQLDelete(sql = "UPDATE newspeed.inquiry_question SET newspeed.inquiry_question.deleted_at = now() WHERE id = ?")
+@Where(clause = "deleted_at is null")
+class InquiryQuestion(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    @Comment("id")
+    val id: Long = 0,
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false, foreignKey = ForeignKey(ConstraintMode.NO_CONSTRAINT))
+    @Comment("사용자 ID")
+    var user: User,
+
+    @Column(name = "title", nullable = false, length = 256)
+    @Comment("문의 내용 제목")
+    var title: String,
+
+    @Column(name = "body", nullable = false, columnDefinition = "text")
+    @Comment("본문 내용")
+    var body: String
+): BaseTimeEntity() {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as InquiryQuestion
+
+        return id == other.id
+    }
+
+    override fun hashCode(): Int {
+        return id.toInt()
+    }
+}
