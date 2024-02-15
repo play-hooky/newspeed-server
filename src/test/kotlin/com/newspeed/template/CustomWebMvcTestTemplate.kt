dc7f4de9ba49ddbf9000ba685598836a557dc77d
white-gyu <white_gyu@naver.com>
2024-02-15 16:08:50 +0900

    test: controller 단위 테스트 위한 authorization header mocking 로직 구현

--- src/test/kotlin/com/newspeed/template/CustomWebMvcTestTemplate.kt
+++ src/test/kotlin/com/newspeed/template/CustomWebMvcTestTemplate.kt
@@ -2,9 +2,11 @@ package com.newspeed.template
 
 import com.newspeed.config.TestAuthConfig
 import com.newspeed.support.MockAllBeanFactoryPostProcessor
+import com.newspeed.support.MockAuthTestExecutionListener
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 import org.springframework.context.annotation.Import
+import org.springframework.test.context.TestExecutionListeners
 
 @WebMvcTest
 @Import(value = [
@@ -14,4 +16,8 @@ import org.springframework.context.annotation.Import
 @Target(AnnotationTarget.CLASS)
 @Retention(AnnotationRetention.RUNTIME)
 @AutoConfigureMockMvc(addFilters = false)
+@TestExecutionListeners(
+    value = [MockAuthTestExecutionListener::class],
+    mergeMode = TestExecutionListeners.MergeMode.MERGE_WITH_DEFAULTS
+)
 annotation class CustomWebMvcTestTemplate
